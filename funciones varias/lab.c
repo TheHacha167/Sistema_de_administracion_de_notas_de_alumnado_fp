432ac025d42ae2f3a046d679ed4393730a67db63
Jesus_hna <thehacha167@gmail.com>
2023-01-17 18:37:54 +0100

    Co-authored-by: tomsalsal <tomsalsal@users.noreply.github.com> Co-authored-by: Os <Oscarz8@users.noreply.github.com>

---
+++ funciones varias/lab.c
@@ -0,0 +1,63 @@
+/*
+TERMINADO Y COMPROBADO
+En este ejercicio se debe implementar un programa en c que,
+dada una cantidad de eurossin céntimos que se ingresa
+como argumento en la ejecución del programa, la desgloseen
+el menor número posible de billetes y monedas. Por ejemplo, 1747 euros sedesglosan en:
+• 3 billetes de 500 euros
+• 1 billete de 200 euros
+• 2 billetes de 20 euros
+• 1 billete de 5 euros
+• 1 moneda de 2 euros
+En el programa a completar se usará un array
+llamado valor, con nueve elementos detipo entero,
+para almacenar en orden descendente el valor de los
+distintos billetes ymonedas de valor mayor o igual a
+1 euro (billetes de 500 euros, de 200 euros, de 100euros,
+de 50 euros, de 20 euros, de 10 euros y de 5 euros; monedas
+de 2 euros y de 1euro).
+*/
+#include <stdio.h>
+#include <stdlib.h> //  función atoi
+#include <string.h> //  función strlen
+
+int main(int argc, char *argv[])
+{
+   int valor[9] = {500, 200, 100, 50, 20, 10, 5, 2, 1}; // Array con los valores de los billetes y monedas
+   char *nombre[9] = {"billetes/monedas de 500 euros", "billetes/monedas de 200 euros", "billetes/monedas de 100 euros",
+                      "billetes de 50 euros", "billetes/monedas de 20 euros", "billetes/monedasde 10 euros",
+                      "billetes/monedas de 5 euros", "billetes/monedas de 2 euros", "billetes/monedas de 1 euro"};
+   int cantidad[9] = {0};
+   int euros;
+   int i;
+
+   // Comprobamos que se han introducido los argumentos correctos
+   if (argc != 2)
+   {
+      printf("DEBE INTRODUCIR 1 ARGUMENTO DE TIPO ENTERO...");
+      // Deberia ser return 1; pero me da error en el comprobador
+      // return 1;
+      return 0;
+   }
+
+   euros = atoi(argv[1]); // Convierte el argumento ingresado a un entero
+   if (euros == 0)
+   {
+      printf("DEBE INTRODUCIR 1 ARGUMENTO DE TIPO ENTERO...");
+      return 0;
+   }
+
+   for (i = 0; i < 9; i++)
+   {
+      cantidad[i] = euros / valor[i]; // Division para obtener la cantidad de billetes o monedas
+      euros %= valor[i];              // Módulo para obtener el resto  y continuar
+   }
+
+   // Salida de los resultados
+   for (i = 0; i < 9; i++)
+   {
+      if (cantidad[i] > 0)
+      {
+         printf(" %d %s\n", cantidad[i], nombre[i]);
+      }
+   }
--- main.c
+++ main.c
@@ -1,6 +1,119 @@
 /*
 
 
+#include <stdio.h>
+
+// Funciones del sistema
+void entradaDatos();
+void calcularNotasMedias();
+void visualizarDatos();
+void modificarEliminarDatos();
+
+int main() {
+    int opcion = 0;
+    while (opcion != 5) {
+        printf("Menu de opciones\n");
+        printf("1. Entrada de datos\n");
+        printf("2. Calcular notas medias\n");
+        printf("3. Visualizar datos\n");
+        printf("4. Modificar y eliminar datos\n");
+        printf("5. Salir\n");
+        printf("Seleccione una opcion: ");
+        scanf("%d", &opcion);
+        switch (opcion) {
+            case 1:
+                entradaDatos();
+                break;
+            case 2:
+                calcularNotasMedias();
+                break;
+            case 3:
+                visualizarDatos();
+                break;
+            case 4:
+                modificarEliminarDatos();
+                break;
+            case 5:
+                printf("Saliendo del sistema...\n");
+                break;
+            default:
+                printf("Opcion incorrecta. Por favor, seleccione una opcion valida.\n");
+        }
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+struct alumno {
+    char dni[10];
+    char nombre[50];
+    char apellidos[100];
+    int curso;
+    char email[50];
+    float asig1;
+    float asig2;
+    float asig3;
+    float asig4;
+    float asig5;
+};
+
+void entradaDatos() {
+    int numAlumnos;
+    printf("Introduce el numero de alumnos: ");
+    scanf("%d", &numAlumnos);
+    struct alumno alumnos[numAlumnos];
+    for (int i = 0; i < numAlumnos; i++) {
+        printf("Introduce el DNI del alumno %d: ", i+1);
+        scanf("%s", alumnos[i].dni);
+        printf("Introduce el nombre del alumno %d: ", i+1);
+        scanf("%s", alumnos[i].nombre);
+        printf("Introduce los apellidos del alumno %d: ", i+1);
+        scanf("%s", alumnos[i].apellidos);
+        printf("Introduce el curso del alumno %d: ", i+1);
+        scanf("%d", &alumnos[i].curso);
+        printf("Introduce el email del alumno %d: ", i+1);
+        scanf("%s", alumnos[i].email);
+        printf("Introduce la nota de la asignatura 1 del alumno %d: ", i+1);
+        scanf("%f", &alumnos[i].asig1);
+        printf("Introduce la nota de la asignatura 2 del alumno %d: ", i+1);
+        scanf("%f", &alumnos[i].asig2);
+        printf("Introduce la nota de la asignatura 3 del alumno %d: ", i+1);
+        scanf("%f", &alumnos[i].asig3);
+        printf("Introduce la nota de la asignatura 4 del alumno %d: ", i+1);
+        scanf("%f", &alumnos[i].asig4);
+        printf("Introduce la nota de la asignatura 5 del alumno %d: ", i+1);
+        scanf("%f", &alumnos[i].asig5);
+    }
+    // Aquí podrías incluir código para ordenar los datos de los alumnos o para guardarlos en un fichero
+}
+
+
+
+void guardarDatos(struct alumno alumnos[], int numAlumnos) {
+    FILE *fp;
+    fp = fopen("alumnos.txt", "w");
+    for (int i = 0; i < numAlumnos; i++) {
+        fprintf(fp, "%s,%s,%s,%d,%s,%f,%f,%f,%f,%f\n", alumnos[i].dni, alumnos[i].nombre, alumnos[i].apellidos, alumnos[i].curso, alumnos[i].email, alumnos[i].asig1, alumnos[i].asig2, alumnos[i].asig3, alumnos[i].asig4, alumnos[i].asig5);
+    }
+    fclose(fp);
+    printf("Los datos de los alumnos se han guardado en el archivo alumnos.txt\n");
+}
+
+
 
 
 */
+// hacer una funcion que pida al usuario un numero#
+#include <stdio.h>
+#include <string.h>
+#include "funciones.h"
